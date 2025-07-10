e05fae9a3ebe9fb1a88ad6761f1d0146b329d854
ArponRoy7 <arponkolkata7@gmail.com>
2025-07-10 17:00:58 +0530

    updated database

--- src/model/user.js
+++ src/model/user.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  name: { type: String },
+  name: { type: String,
+    required : true,
+    unique:true,
+   },
   age: { type: Number },
-  gender: { type: String },
-  email: { type: String },
-  password: { type: String }
-});
+  gender: { type: String,
+    validate(value)
+{
+  if(!["male","female","others"].includes(value))
+  {
+    throw new Error("gender is wrong ");
+  }
+}  },
+  email: { type: String
+   },
+  password: { type: String },
+  photourl:{type : String,default:"default value "},
+},
+{
+  timestamps:true,
+}
+);
 
 const User = mongoose.model('User', schema);
 module.exports = User;
