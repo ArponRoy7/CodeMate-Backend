ed6cb243c0015aa054d551b716c9f25157df252b
ArponRoy7 <arponkolkata7@gmail.com>
2025-07-11 17:23:14 +0530

    cookies added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,97 @@ const express = require('express');
 const app = express();
 const { connectDB } = require('./config/database');
 const User = require('./model/user');
+const {adminAuth} = require("/home/arpon-roy/Desktop/WebDevCodes/Namaste Node JS/Season_2/DevTinderBackend/src/middleware/auth.js")
 const {validatesignupdata}=require("./utils/validations");
 const bcrypt = require('bcrypt');
+const cookieparser = require("cookie-parser");
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
+app.use(cookieparser());
+//signup
+app.post("/signup", async (req, res) => {
+  try {
+    //validator
+    validatesignupdata(req); // custom validator
+    //encrypt the password
+    const { name, email, password } = req.body;
+const passwordhash = await bcrypt.hash(password, 10);
+console.log(passwordhash); // Print hashed password
+
+const user = new User({
+  name,
+  email,
+  password: passwordhash, // Store hashed password
+});
+
+
+    console.log("User Details:");
+    console.log(req.body);
+    await user.save(); // <- this actually saves the user to DB
+
+    res.status(201).send("User created successfully");
+    
+  } catch (err) {
+    console.error(err.message); 
+    if (err.name === "ValidationError" || err.message?.includes("valid")) {
+      res.status(400).send(err.message);
+    } else {
+      res.status(500).send("Error creating user");
+    }
+  }
+  
+});
+//profile view api
+app.get("/profile",adminAuth,async (req,res)=>
+{
+  try {
+    
+   console.log(req.loginuser.name);
+    res.send("profile view allowed");
+  } catch (error) {
+    res.status(404).send("Error  !!!"+error.message);
+  }
+}
+)
+app.get("/test",adminAuth,async(req,res)=>
+{
+  try {
+    
+     res.send("profile view allowed");
+   } catch (error) {
+     res.status(404).send("Error  !!!"+error.message);
+   }
+})
+//log in api
+app.post("/login",async(req,res)=>
+{
+  try{
+    const{email,password}=req.body;
+    const user = await User.findOne({email:email});
+    if(!user)
+    {
+      throw new Error("Invalid Cred");
+    }
+    const ispasswordvalid = await bcrypt.compare(password, user.password);
+
+    if(ispasswordvalid)
+    {
+//create token
+const token = await jwt.sign({_id:user._id},"arpon123",{expiresIn:"1d"});
+res.cookie("token",token);
+console.log(token);
+      res.send("Log in successfull");
+    }
+    else
+    throw new Error("Password not valid");
+  }
+  catch(err)
+  {
+    res.status(404).send("Error");
+  }
+})
+
 //updating
 app.patch("/user", async (req, res) => {
   const userid = req.body._id;
@@ -34,32 +121,6 @@ app.patch("/user", async (req, res) => {
     res.status(400).send("Something Went Wrong");
   }
 });
-//log in api
-app.post("/login",async(req,res)=>
-{
-  try{
-    const{email,password}=req.body;
-    const user = await User.findOne({email:email});
-    if(!user)
-    {
-      throw new Error("Invalid Cred");
-    }
-    const ispasswordvalid = await bcrypt.compare(password, user.password);
-
-    if(ispasswordvalid)
-    {
-      res.send("Log in successfull");
-    }
-    else
-    throw new Error("Password not valid");
-  }
-  catch(err)
-  {
-    res.status(404).send("Error");
-  }
-})
-
-
 //deleteing
 app.delete("/user",async(req,rep)=>
 {
@@ -98,38 +159,6 @@ app.get("/feed",async(req,res)=>
     res.status(404).send("error happed");
   }
 })
-//signup
-app.post("/signup", async (req, res) => {
-  try {
-    //validator
-    validatesignupdata(req); // custom validator
-    //encrypt the password
-    const { name, email, password } = req.body;
-const passwordhash = await bcrypt.hash(password, 10);
-console.log(passwordhash); // Print hashed password
-
-const user = new User({
-  name,
-  email,
-  password: passwordhash, // Store hashed password
-});
-
-
-    console.log("User Details:");
-    console.log(req.body);
-    await user.save(); // <- this actually saves the user to DB
-
-    res.status(201).send("User created successfully");
-  } catch (err) {
-    console.error(err.message); 
-    if (err.name === "ValidationError" || err.message?.includes("valid")) {
-      res.status(400).send(err.message);
-    } else {
-      res.status(500).send("Error creating user");
-    }
-  }
-  
-});
 
 
 connectDB()
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,22 @@
-const adminAuth = (req, res, next) => { 
-    console.log("Admin auth is getting checked!!"); 
-    const token = "xyzaaa"; 
-    const isAdminAuthorized = token === "xyz"; 
-    if (!isAdminAuthorized) { 
-        res.status(401).send("Unauthorized request"); 
-    } else { 
-        next(); 
-    } 
+const User = require("/home/arpon-roy/Desktop/WebDevCodes/Namaste Node JS/Season_2/DevTinderBackend/src/model/user.js");
+const jwt = require('jsonwebtoken');
+const adminAuth = async (req, res, next) => { 
+    try {
+    const cookie=req.cookies;
+    const {token}=cookie;
+    if(!token)
+      throw new Error("Invalid token ");
+    //validate token 
+    const decoded =await jwt.verify(token,"arpon123");
+    const {_id}=decoded;
+    const loginuser = await User.findById(_id);
+    req.loginuser=loginuser;
+    if(!loginuser)
+      throw new Error("No user found");
+    next();
+    } catch (error) {
+        console.log("Error : "+error.message);
+        res.send("Error occurs");
+    }
 };
 module.exports = {adminAuth};
